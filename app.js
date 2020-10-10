87d396dd3ac7c90ff8e58610f80c6c6caa95a122
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-10-10 19:41:47 +0530

    Added route and blog component

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    res.send('Node Application')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
 
 module.exports = app;
